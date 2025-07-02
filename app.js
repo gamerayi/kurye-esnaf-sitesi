b8fb03fa916553b9d7bc9b245fc8ee020bb95287
gamerayi <dehabilsem@gmail.com>
2025-07-03 00:43:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -31,3 +31,53 @@ function selectType(type) {
     `;
   }
 }
+function kaydetEsnaf() {
+  const inputs = document.querySelectorAll("#kayitFormu input");
+  const veri = {
+    dukan_adi: inputs[0].value,
+    konum: inputs[1].value,
+    paket_turu: inputs[2].value,
+    telefon: inputs[3].value,
+    sifre: inputs[4].value
+  };
+
+  fetch(`${SUPABASE_URL}/rest/v1/esnaflar`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(veri)
+  })
+  .then(res => {
+    if (res.ok) {
+      alert("Esnaf kaydı başarılı!");
+    } else {
+      alert("Esnaf kaydı başarısız!");
+    }
+  });
+
+  return false;
+}
+
+function kaydetKurye() {
+  const inputs = document.querySelectorAll("#kayitFormu input");
+  const veri = {
+    isim: inputs[0].value,
+    telefon: inputs[1].value,
+    motor_bilgisi: inputs[2].value,
+    sifre: inputs[3].value
+  };
+
+  fetch(`${SUPABASE_URL}/rest/v1/kuryeler`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(veri)
+  })
+  .then(res => {
+    if (res.ok) {
+      alert("Kurye kaydı başarılı!");
+    } else {
+      alert("Kurye kaydı başarısız!");
+    }
+  });
+
+  return false;
+}
